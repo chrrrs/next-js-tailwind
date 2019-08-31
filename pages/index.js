42efa1016e62d40d319c71500d756188c2505da7
Chris <chriseckert.ce@gmail.com>
2019-08-31 08:24:03 +0200

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import "../assets/styles.css";
+
+const index = () => {
+  return (
+    <div>
+      <p>
+        Hello from Next.js and{" "}
+        <span className='text-teal-400'>TailwindCSS</span>
+      </p>
+    </div>
+  );
+};
+
+export default index;
